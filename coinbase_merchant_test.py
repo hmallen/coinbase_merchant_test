8389d0b854cbb1979380dc42cac54656933b143d
hmallen <allenhm@gmail.com>
2017-08-09 23:33:05 -0500

    Initial commit.

---
+++ coinbase_merchant_test.py
@@ -0,0 +1,26 @@
+#!/usr/env python
+
+import ConfigParser
+from coinbase.wallet.client import Client
+
+Config = ConfigParser.ConfigParser()
+
+
+def ConfigSectionMap(section):
+    dict1 = {}
+    options = Config.options(section)
+    for option in options:
+        try:
+            dict1[option] = Config.get(section, option)
+            if dict1[option] == -1:
+                DebugPrint("skip: %s" % option)
+        except:
+            print("exception on %s!" % option)
+            dict1[option] = None
+    return dict1
+
+
+coinbase_api = ConfigSectionMap('Credentials')['api']
+coinbase_secret = ConfigSectionMap('Credentials')['secret']
+
+#COINBASE
